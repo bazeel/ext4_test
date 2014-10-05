277d0b9ebf11a17ece51d3234a496e074418ea52
v.zaicev <vasiliyzaycev1989@gmail.com>
2014-10-05 22:16:58 +0400

    remove commented code

--- app.js
+++ app.js
@@ -11,17 +11,9 @@ Ext.application({
         'ItemController'
     ],
 
-    //stores : ['Items'],
-    //models : ['Item'],
-
     launch: function() {
         Ext.create('App.view.Viewport', {
             renderTo: 'App'
-            //height: 250,
-            //width: 500,
-            //margin: '5 5 5 5 ',
-            //layout: 'fit',
-            //items: Ext.create('App.view.Viewport')
         });
     }
 });
